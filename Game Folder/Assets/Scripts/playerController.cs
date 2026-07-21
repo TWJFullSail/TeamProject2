d64f0872e9800bd7ae291cccef978d9829006c31
Timothy Joseph <TWJoseph@student.fullsail.edu>
2026-07-21 07:39:09 -0400

    Update playerController.cs
    Updated and merged the playerController so hopefully that resolves the issues with the current build. This version should now have all the features covered by the lectures, plus any changes/features we have added/updated.

--- Game Folder/Assets/Scripts/playerController.cs
+++ Game Folder/Assets/Scripts/playerController.cs
@@ -1,9 +1,9 @@
-using System.Collections;
 using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 
-public class playerController : MonoBehaviour, IDamage
+public class playerController : MonoBehaviour, IDamage, IPickupGun
 {
     [SerializeField] CharacterController controller;
     [SerializeField] LayerMask ignoreLayer;
@@ -45,9 +45,9 @@ public class playerController : MonoBehaviour, IDamage
         if (!gamemanager.instance.isPaused)
         {
             movement();
-        }
 
-        sprint();
+            sprint();
+        }
     }
 
     void movement()
@@ -62,21 +62,21 @@ public class playerController : MonoBehaviour, IDamage
         controller.Move(moveDir.normalized * speed * Time.deltaTime);
 
         jump();
-
         controller.Move(playerVel * Time.deltaTime);
         playerVel.y -= gravity * Time.deltaTime;
 
         shootTimer += Time.deltaTime;
         if (Input.GetButton("Fire1") && gunInv.Count > 0 && gunInv[gunInvPos].ammoCur > 0 && shootTimer > gunInv[gunInvPos].shootRate)
         {
-
             shoot();
         }
 
+        /*
         if (gunInv.Count > 0)
         {
             Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * gunInv[gunInvPos].shootDist, Color.red);
         }
+        /**/
 
         selectGun();
         reload();
@@ -84,12 +84,10 @@ public class playerController : MonoBehaviour, IDamage
 
     void sprint()
     {
-        if (Input.GetButtonDown("Sprint"))
-        {
+        if(Input.GetButtonDown("Sprint")) {
             speed *= sprintMod;
         }
-        else if (Input.GetButtonUp("Sprint"))
-        {
+        else if(Input.GetButtonUp("Sprint")) {
             speed /= sprintMod;
         }
     }
@@ -109,20 +107,28 @@ public class playerController : MonoBehaviour, IDamage
         gunInv[gunInvPos].ammoCur--;
 
         RaycastHit hit;
-
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, gunInv[gunInvPos].shootDist, ~ignoreLayer))
         {
-
             //Debug.Log(hit.collider.name);
-
-            Instantiate(gunInv[gunInvPos].hitEffect, hit.point, Quaternion.identity);
+            if(gunInv[gunInvPos].hitEffect != null)
+            {
+                Instantiate(gunInv[gunInvPos].hitEffect, hit.point, Quaternion.identity);
+            }
 
             IDamage dmg = hit.collider.GetComponent<IDamage>();
-
-            if (dmg != null)
+            if (dmg != null) 
             {
                 dmg.takeDamage(gunInv[gunInvPos].shootDamage);
             }
+        }
+    }
+
+    void reload()
+    {
+        if (Input.GetButtonDown("Reload") && gunInv.Count > 0 && gunInv[gunInvPos].ammoCur < gunInv[gunInvPos].ammoMax)
+        {
+            gunInv[gunInvPos].ammoCur = gunInv[gunInvPos].ammoMax;
+            updateAmmoUI();
         }
     }
 
@@ -132,18 +138,10 @@ public class playerController : MonoBehaviour, IDamage
         updatePlayerUI();
         StartCoroutine(flashDamage());
 
-        if (HP <= 0)
+        if(HP <= 0)
         {
             gamemanager.instance.youLose();
 
-        }
-    }
-
-    void reload()
-    {
-        if (Input.GetButtonDown("Reload") && gunInv.Count > 0)
-        {
-            gunInv[gunInvPos].ammoCur = gunInv[gunInvPos].ammoMax;
         }
     }
 
@@ -167,37 +165,30 @@ public class playerController : MonoBehaviour, IDamage
 
     public void getGunStats(gunStats gun)
     {
-
         gunInv.Add(gun);
         gunInvPos = gunInv.Count - 1;
 
-        changeGun();
-
+        changeGun();        
     }
 
     void changeGun()
     {
-        foreach (Transform child in gunModel.transform)
-        {
-            Destroy(child.gameObject);
-        }
-
-        Instantiate(gunInv[gunInvPos].gunModel, gunModel.transform);
+        gunModel.GetComponent<MeshFilter>().sharedMesh = gunInv[gunInvPos].gunModel.GetComponent<MeshFilter>().sharedMesh;
+        gunModel.GetComponent<MeshRenderer>().sharedMaterial = gunInv[gunInvPos].gunModel.GetComponent<MeshRenderer>().sharedMaterial;
     }
 
     void selectGun()
     {
-        if (Input.GetAxis("Mouse ScrollWheel") > 0 && gunInvPos < gunInv.Count - 1)
+        if(Input.GetAxis("Mouse ScrollWheel") > 0 && gunInvPos < gunInv.Count - 1)
         {
             gunInvPos++;
             changeGun();
         }
-
         else if (Input.GetAxis("Mouse ScrollWheel") < 0 && gunInvPos > 0)
         {
             gunInvPos--;
             changeGun();
-        }
+        }        
     }
 
     public void spawnPlayer()
